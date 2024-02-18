e2a1bdf590c15d965319642e27d8d7478fd8c4a4
unyxium <unyxium@gmail.com>
2024-02-18 23:39:59 +1100

    generalised

--- encode_examples.py
+++ encode_examples.py
@@ -1,22 +1,33 @@
-import converter
+"""Get all files in the examples folder, encode and then decode."""
 
+import os
+import converter
 import json
 
-FILES = ['code_errors.json', 'analysed_all.json', 'griffpatch.json', 'turbowarp-addon-settings.json', 'comments.json', 'cols.json', 'model1.json']
-
-
-for file in FILES:
-    # load
-    with open(f'examples/{file}') as f:
-        data = json.loads(f.read())
-
-    output = converter.encode(data)
-
-    with open(f'examples/converted/{file}.txt', 'w') as f:
-        f.writelines([str(o)+'\n' for o in output])
-
-
-    #
+def list_files(directory):
+    """Get a list of all file names in a directory."""
     
-    with open(f'examples/converted/{file}.txt.json', 'w') as f:
-        f.write(json.dumps(converter.decode(output), indent=2))
+    files = os.listdir(directory) # get all contents in the directory
+    files = [file for file in files if os.path.isfile(os.path.join(directory, file))] # filter directories
+    return files
+
+if True:
+    DIRECTORY = 'examples/'
+    for file_name in list_files(DIRECTORY):
+        with open(DIRECTORY+file_name) as f:
+            data = json.loads(f.read())
+
+        output = converter.encode(data)
+
+        with open(f'{DIRECTORY}encoded/{file_name}.txt', 'w') as f:
+            f.writelines([str(o)+'\n' for o in output])
+
+if True:
+    DIRECTORY = 'examples/encoded/'
+    for file_name in list_files(DIRECTORY):
+        with open(DIRECTORY+file_name) as f:
+            data = f.readlines()
+            data = [d[0:-1] for d in data] # remove newline
+
+        with open(f'{DIRECTORY}decoded/{file_name}.json', 'w') as f:
+            f.write(json.dumps(converter.decode(data), indent=2))
